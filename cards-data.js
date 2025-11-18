034a009e899683cef13307f1f1a83a427c2bcc33
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-11-18 12:50:35 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/Thats not my neighbor/index.html', imgSrc: 'thats not my neighbor.jpeg', name: 'Thats not my neighbor' , 'page': 5, 'id': 985 },
   { href: './html/Thelast.io/index.html', imgSrc: 'thelast.io.png', name: 'Thelast.io' , 'page': 5, 'id': 986 },
   { href: './html/SSF2/index.html', imgSrc: 'SSF2.png', name: 'SSF2' , 'page': 5, 'id': 992 },
   { href: './html/Road of the dead/index.html', imgSrc: 'road of the dead.jpeg', name: 'Road of the dead' , 'page': 5, 'id': 995 },
